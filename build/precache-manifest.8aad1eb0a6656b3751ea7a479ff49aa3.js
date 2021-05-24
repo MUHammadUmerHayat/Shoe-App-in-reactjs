d7fbddca8d47f0b3d4de70a8c69fe36e96a5975a
Muhammad Umer <40881824+MUHammadUmerHayat@users.noreply.github.com>
2021-05-24 20:40:23 +0500

    make build file

---
+++ build/precache-manifest.8aad1eb0a6656b3751ea7a479ff49aa3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77a8014d20d0cf0503ddfa878ba6f484",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2cc5dbce716bc27a887b",
+    "url": "/static/css/main.22fc1cf7.chunk.css"
+  },
+  {
+    "revision": "42e5dd30c5389cdb240b",
+    "url": "/static/js/2.a86773f5.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.a86773f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cc5dbce716bc27a887b",
+    "url": "/static/js/main.d8b6a6e1.chunk.js"
+  },
+  {
+    "revision": "fd94350abc9cc4615575",
+    "url": "/static/js/runtime-main.ec96c9be.js"
+  }
+]);
